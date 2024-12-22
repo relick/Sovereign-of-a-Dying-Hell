b64209fa91836844f4fa96e1e7cb3ee34d932dea
relick <relick@users.noreply.github.com>
2024-12-22 15:56:29 +0000

    Get everything ready for debugging

---
+++ build_release.bat
@@ -0,0 +1,16 @@
+:: Build all games
+:: The library must be compiled with ENABLE_NEWLIB and ENABLE_CPLUSPLUS set to 1
+
+ECHO off
+SET "GDK=%cd:\=/%/../../SDKs/SGDKplusplus"
+SET "GDK_WIN=%cd%/../../SDKs/SGDKplusplus"
+SET "XGCC_WIN=%GDK_WIN%\x68k-gcc\bin"
+SET "PATH=%GDK_WIN%\bin;%XGCC_WIN%;%PATH%"
+
+set DIRS=games\proof-of-concept
+
+for /D %%i in (%DIRS%) do (
+    echo "%%i"
+    %GDK_WIN%\bin\make -C "%%i" release -f %GDK_WIN%\makefilecpp.gen
+    %GDK_WIN%\bin\make -C "%%i" distclean -f %GDK_WIN%\makefilecpp.gen 
+)  
