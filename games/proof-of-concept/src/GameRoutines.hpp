427e4488d52d31f1ce0413d74723c28437031b29
relick <relick@users.noreply.github.com>
2025-01-24 16:36:42 +0000

    Set WorldRoutine to suspend_always as isn't bugged

--- games/proof-of-concept/src/GameRoutines.hpp
+++ games/proof-of-concept/src/GameRoutines.hpp
@@ -7,7 +7,7 @@ namespace Game
 
 using DMARoutine = Coroutine<struct DMARoutineTag, std::suspend_never>;//std::suspend_always>;
 
-using WorldRoutine = Coroutine<struct WorldRoutineCoroTag, std::suspend_never>;//std::suspend_always>;
+using WorldRoutine = Coroutine<struct WorldRoutineCoroTag, std::suspend_always>;
 
 using SceneRoutine = Coroutine<struct SceneRoutineTag, std::suspend_never>;
 
