4c6296b5610bb3bb7c4f235389a86e3ef4a02026
relick <relick@users.noreply.github.com>
2025-02-22 11:03:21 +0000

    Update animator to support looping to non-0 index

--- game/src/CharacterAnimator.cpp
+++ game/src/CharacterAnimator.cpp
@@ -29,29 +29,37 @@ void CharacterAnimator::Update
 			return;
 		}
 
-
 		++m_context->m_ticks;
 		if (m_context->m_ticks >= m_context->m_currentFrameTicks)
 		{
-			++m_context->m_currentFrameIndex;
-			if (m_context->m_currentFrameIndex >= m_context->m_pose->m_animation.size())
+			// Step to next frame, using the index provided or just incrementing
+			AnimFrame const& prevFrame = m_context->m_pose->m_animation[m_context->m_currentFrameIndex];
+			if (prevFrame.m_nextIndex < m_context->m_pose->m_animation.size())
 			{
-				m_context->m_currentFrameIndex = 0;
+				m_context->m_currentFrameIndex = prevFrame.m_nextIndex;
+			}
+			else
+			{
+				++m_context->m_currentFrameIndex;
+				if (m_context->m_currentFrameIndex >= m_context->m_pose->m_animation.size())
+				{
+					m_context->m_currentFrameIndex = 0;
+				}
 			}
 
-			AnimFrame const& animFrame = m_context->m_pose->m_animation[m_context->m_currentFrameIndex];
+			AnimFrame const& curFrame = m_context->m_pose->m_animation[m_context->m_currentFrameIndex];
 
 			m_context->m_ticks = 0;
-			m_context->m_currentFrameTicks = animFrame.m_duration.Get();
+			m_context->m_currentFrameTicks = curFrame.m_duration.Get();
 
 			io_game.QueueFunctionTask(Tiles::SetMap_SubFull(
 				VDP_BG_A,
-				animFrame.m_tilemap->tilemap,
-				animFrame.m_tilemap->w,
-				animFrame.m_tilemap->h,
+				curFrame.m_tilemap->tilemap,
+				curFrame.m_tilemap->w,
+				curFrame.m_tilemap->h,
 				m_context->m_baseTile,
-				animFrame.m_xOffset,
-				animFrame.m_yOffset
+				curFrame.m_xOffset,
+				curFrame.m_yOffset
 			), TaskPriority::Animations);
 		}
 	}
