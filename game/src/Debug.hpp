19255a86601a004f9f2f07081006a97b437e98cf
relick <relick@users.noreply.github.com>
2025-04-25 02:31:21 +0100

    Add 'Assert' macro, and a way to breakpoint on Error/Assert

--- game/src/Debug.hpp
+++ game/src/Debug.hpp
@@ -2,8 +2,17 @@
 
 #include "Declare.hpp"
 
+#include <csignal>
+
 #if DEBUG
-#define Error(...) kprintf(__VA_ARGS__)
+inline u16 volatile s_debugBreakpointHook = 0;
+inline void DebugBreakpointHook()
+{
+	s_debugBreakpointHook = 1;
+}
+#define Error(...) kprintf(__VA_ARGS__); DebugBreakpointHook()
+#define Assert(CHECK, ...) if (!(CHECK)) { Error(__VA_ARGS__); }
 #else
 #define Error(...)
+#define Assert(CHECK, ...)
 #endif
